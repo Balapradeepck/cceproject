fbfc513b91d2ac948bacd1f659161f050fb2bcfd
Balapradeepck <44576094+Balapradeepck@users.noreply.github.com>
2018-11-10 18:19:53 +0530

    Update pre_process.py
    We have completed the below items as part of this code
    1. Assigning the na values for missing and defaults values like ["NA","N/A","na","n/a"]
    2. Making sure to have all the data  is in float
    3. If method is either given as drop or not provided then its been defaulted to have zero for all NaN's
    4. If method is given as avg then its been defaulted to have average of the column for all NaN's

--- preprocess/pre_process.py
+++ preprocess/pre_process.py
@@ -1,17 +1,26 @@
 import pandas
 
-def clean_data(location, method):
-    """
-
-    :param location: String location of raw file
-    :return: data frame of cleaned data
-    """
-
-    df_raw = pandas.read_csv(location)
-
-    """
-    add logic to clean data
-    """
-    df_clean = df_raw #just for testing
-
+def clean_data(location,method="drop"):
+# assigning the NA values for default values
+    missingvalues=["NA","N/A","na","n/a"]
+    df_raw = pd.read_csv(location,na_values=missingvalues)
+#   print the raw data after defaulting the na values
+    print (df_raw)
+#  converting all the data to float and cleaning any non numeric values
+    df_raw=df_raw.replace('[^0-9]','',regex=True).astype(float)
+#   print the data with only numeric values 
+    print (df_raw)
+# method called in if loop for drop or avg
+    if method=="drop":
+        df_clean=df_raw.fillna(0)
+        print("Printing the values after filling the NA values with zero's")
+    elif method=="avg":
+        df_clean=df_raw.fillna(df_raw.mean())
+        print("Printing the values after filling the NA values with average of the column")
+    print (df_clean)
+    del (df_raw)
     return df_clean
+# Test execution
+#clean_data("Location","drop")
+#clean_data("Location","avg")
+#clean_data("Location")
